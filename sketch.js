bd16ea6e87444ecb13d29b70b58580f2d9371374
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2025-03-27 21:03:10 -0500

    converted file and added button

--- sketch.js
+++ sketch.js
@@ -1,11 +1,24 @@
-let samples, button1, bgImg;
+let samples, button, bgImg;
 
-function preload(){
+function preload() {
   bgImg = loadImage("media/images/nola_group.jpg");
+
+  samples = new Tone.Players({
+    bday: "media/audio/birthday_2021.mp3",
+  }).toDestination();
 }
 
 function setup() {
   createCanvas(windowWidth, 600);
+  
+  let vw = windowWidth / 2;  
+
+  button = createButton("Happy Birthday");
+  button.position(vw - 50, 300);  
+  button.mousePressed(() => {
+    Tone.start();
+    samples.player("bday").start();
+  });
 }
 
 function draw() {
