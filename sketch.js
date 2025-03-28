ce0eebaa71e76134e61bf564c25de582d81ad607
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2025-03-27 21:39:47 -0500

    changed files

--- sketch.js
+++ sketch.js
@@ -4,17 +4,16 @@ function preload() {
   bgImg = loadImage("media/images/nola_group.jpg");
 
   samples = new Tone.Players({
-    bday: "media/audio/birthday_2021.mp3",
+    bday: "media/audio/birthday_2021.mp3"
   }).toDestination();
 }
 
 function setup() {
   createCanvas(windowWidth, 600);
-  
-  let vw = windowWidth / 2;  
 
+  let vw = windowWidth / 2;
   button = createButton("Happy Birthday");
-  button.position(vw - 50, 300);  
+  button.position(vw, 300);  
   button.mousePressed(() => {
     Tone.start();
     samples.player("bday").start();
